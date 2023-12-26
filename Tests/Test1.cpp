a9abf949144bf61994ba74b490c008fc71be2dbb
Jeffcaruso <50027834+Jeffcaruso@users.noreply.github.com>
2023-12-25 20:01:29 -0800

    Check enrollments done. *SD*
    Should be close to fully functional now.
    
    Probably still have some more fixes that need to be made, but should be getting close with this commit.

--- Tests/Test1.cpp
+++ Tests/Test1.cpp
@@ -25,7 +25,7 @@ TEST(Test1, testingOutAddContains)
 	ASSERT_FALSE(es.addUniversity("UWB"));
 	ASSERT_TRUE(es.addUniversity("UW Seattle"));
 	ASSERT_TRUE(es.addUniversity("UW Tacoma"));
-	ASSERT_FALSE(es.setCurrentUniversity("UWx"))
+	ASSERT_FALSE(es.setCurrentUniversity("UWx"));
 	ASSERT_TRUE(es.setCurrentUniversity("UWB"));
 	string expectedAns = "UWB";
 	ASSERT_EQ(es.getUniversityName(), expectedAns);
@@ -40,15 +40,10 @@ TEST(Test1, testingOutAddContains)
 
 	// Check courses (use expect in gTest)
 	// Expect b/c, if these fail, later items could still at least be somewhat informative.
-	// assert(!es.addCourse(999, "CSS342"));
 	EXPECT_FALSE(es.addCourse(999, "CSS342"));
-	// assert(!es.addCourse(1070, "CSS3422"));
 	EXPECT_FALSE(es.addCourse(1070, "CSS3422"));
-	// assert(!es.isInCourse(1070, "CSS342"));
 	EXPECT_FALSE(es.isInCourse(1070, "CSS342"));
-	// assert(es.addCourse(1070, "CSS342"));
 	EXPECT_TRUE(es.addCourse(1070, "CSS342"));
-	// assert(es.dropCourse(1070, "CSS343"));
 	EXPECT_TRUE(es.dropCourse(1070, "CSS343"));
 	expectedAns = "Data Structures, Algorithms, and Discrete Mathematics I";
 	EXPECT_EQ(es.getCourseTitle("CSS342"), expectedAns);
@@ -56,16 +51,18 @@ TEST(Test1, testingOutAddContains)
 
 	// Check enrollments (use expect in gTest)
 	// Expect b/c, if these fail, later items could still at least be somewhat informative.
-	// assert(es.getEnrolledCourses(1070) == "[CSS110, CSS173, CSS342]");
-	// string c342byLastName =
-	//     "[Collins, Cassandra (1070), Evans, Juan (1401), Gonzalez, Holly
-	//     (1223), " "Henderson, Daniel (1364), Taylor, Alexander (1986),
-	//     Thompson, Christine "
-	//     "(1386), Watson, Alex (1211)]";
-	// assert(es.getClassListByLastName("CSS342") == c342byLastName);
-	// string c342byID =
-	//     "[Collins, Cassandra (1070), Watson, Alex (1211), Gonzalez, Holly "
-	//     "(1223), Henderson, Daniel (1364), Thompson, Christine (1386), Evans, "
-	//     "Juan (1401), Taylor, Alexander (1986)]";
-	// assert(es.getClassListByID("CSS342") == c342byID);
+	expectedAns = "[CSS110, CSS173, CSS342]";
+	EXPECT_EQ(es.getEnrolledCourses(1070), expectedAns);
+	string c342byLastName =
+	    "[Collins, Cassandra (1070), Evans, Juan (1401), Gonzalez, Holly
+	    (1223), " "Henderson, Daniel (1364), Taylor, Alexander (1986),
+	    Thompson, Christine "
+	    "(1386), Watson, Alex (1211)]";
+	EXPECT_EQ(es.getClassListByLastName("CSS342"), c342byLastName);
+	string c342byID =
+	    "[Collins, Cassandra (1070), Watson, Alex (1211), Gonzalez, Holly "
+	    "(1223), Henderson, Daniel (1364), Thompson, Christine (1386), Evans, "
+	    "Juan (1401), Taylor, Alexander (1986)]"
+	EXPECT_EQ(es.getClassListByID("CSS342"), c342byID);
+	
 }
